c4439474e1a1f6350cbe6a9a019c3c5d64bdef4b
taka-0303 <87518654+taka-0303@users.noreply.github.com>
2021-10-04 16:44:29 +0900

    ユーザー管理の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_read, presence: true
-  validates :first_name_read, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください"} do
+    validates :last_name_read, presence: true
+    validates :first_name_read, presence: true
+  end
   validates :birth_day, presence: true
+  
 end
