5c999bf0cfaf6867fb8582fe7919a40cb97eddf0
taka-0303 <87518654+taka-0303@users.noreply.github.com>
2021-10-08 12:54:14 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
