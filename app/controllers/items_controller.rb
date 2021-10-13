4027dd75f90a27f1baf26b3873fd75411d35911c
taka-0303 <87518654+taka-0303@users.noreply.github.com>
2021-10-13 10:25:26 +0900

    商品編集ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -21,10 +22,28 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(@item)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 end
 
 private
 
 def item_params
   params.require(:item).permit(:image, :item_name, :text, :category_id, :condition_id, :commission_id, :area_id, :delivery_id, :price,).merge(user_id: current_user.id)
+end
+
+def contributor_confirmation
+  @item = Item.find(params[:id])
+  redirect_to root_path unless current_user == @item.user
 end
