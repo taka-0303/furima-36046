7f0e0b9ef8e78043d1142026cb744dd068db3a89
taka-0303 <87518654+taka-0303@users.noreply.github.com>
2021-10-15 21:17:55 +0900

    コントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :purchases, only: [:index, :create]
+  end
   
 end
