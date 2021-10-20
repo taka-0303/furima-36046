9b38b6f841ed051c27f6dd5ad12263ea310470f9
taka-0303 <87518654+taka-0303@users.noreply.github.com>
2021-10-20 12:32:06 +0900

    クレジット決済の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_shipping = PurchaseShipping.new(purchase_params)
     if @purchase_shipping.valid?
+      pay_item
       @purchase_shipping.save
       redirect_to root_path
     else
@@ -19,6 +20,15 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_shipping).permit(:postal_code, :area_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_shipping).permit(:postal_code, :area_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_e924822b617b6706c9124b68"  
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy' 
+      )
   end
 end
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -2,7 +2,7 @@ class PurchaseShipping
   
 
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipalities, :address, :building_name, :cuilding, :telephone_number
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipalities, :address, :building_name, :cuilding, :telephone_number, :token
 
   validates :user_id, presence: true
   validates :item_id, presence: true
